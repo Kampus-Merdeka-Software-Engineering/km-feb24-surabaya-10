1bd5d24ad0a21682ca2acf23decf4ac18dd3f4c6
RIF-creativity <126873296+RIF-creativity@users.noreply.github.com>
2024-05-23 18:59:33 +0700

    filter chart

--- js/chart.js
+++ js/chart.js
@@ -1,49 +1,90 @@
+
 document.addEventListener('DOMContentLoaded', () => {
+    const SaleChart = document.getElementById("sale").getContext("2d");
     const loadingOverlay = document.getElementById('loadingOverlay');
     const content = document.getElementById('content');
-
+    const filter = document.getElementById('filter');
     // Tampilkan animasi loading saat halaman dimuat
     loadingOverlay.style.display = 'flex'; // Tampilkan overlay
 
-
+    // Fungsi pengambil data dari JSON
     let data = [];
-    fetch('http://localhost:1000/data')
+    fetch('http://localhost:1000/dataset')
         .then(response => response.json())
         .then(fetchedData => {
             loadingOverlay.style.display = 'none'; // Sembunyikan overlay
             content.style.display = 'block'; // Tampilkan konten utama
             data = fetchedData; // Simpan data yang diambil dari server
-            // Buat chart setelah data tersedia
-            createChart(countDataByMonthAndYear(data));
+            dwellings = DweliingsData(data);
+            let chart = LineChart(SaleChart, countDataByMonthAndYear(data), countDataByMonthAndYear(dwellings));
+            filter.addEventListener('change', () => {
+                let value = filter.value;
+                const filteredData = countDataByMonthAndYear(FilterData(data, value));
+                const filteredDwellingsData = countDataByMonthAndYear(FilterData(dwellings, value));
+                console.log(filteredData);
+                chart.data.labels = Object.keys(filteredData); // Perbarui labels
+                chart.data.datasets[0].data = Object.values(filteredData); // Perbarui data
+                chart.data.datasets[1].data = Object.values(filteredDwellingsData); // Perbarui data
+                chart.update(); // Perbarui chart
 
-            // Sembunyikan animasi loading setelah data selesai dimuat
+            });
+            // Panggil fungsi untuk membuat chart setelah data diterima
         })
         .catch(error => {
             console.error('Error fetching data:', error);
             loadingOverlay.style.display = 'none';
         });
+});
+// Fungsi untuk membuat line chart
+function LineChart(ChartID, AllData, DweliingsData) {
 
-    //fungsi membuat nama bulan
-    function getMonthName(monthNumber) {
-        const months = [
-            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-        ];
-        return months[monthNumber - 1]; // Bulan dimulai dari indeks 0
-    }
+    const labels = Object.keys(AllData); // Ambil kunci dari objek AllData sebagai label
+    const dataValues = Object.values(AllData); // Ambil nilai dari objek AllData sebagai data
+    const dataDwellings = Object.values(DweliingsData); // Ambil nilai dari objek AllData sebagai data
 
-  // Fungsi untuk menghitung jumlah data dengan bulan dan tahun yang sama
+    let ChartLine = new Chart(ChartID, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'All sale',
+                    data: dataValues,
+                    fill: false,
+                    borderColor: '#1c283337',
+                },
+                {
+                    label: 'Dwellings sale',
+                    data: dataDwellings,
+                    fill: false,
+                    borderColor: '#FFDE3E',
+                }
+            ]
+        }
+    });
+    return ChartLine;
+}
+// Fungsi membuat nama bulan
+function getMonthName(monthNumber) {
+    const months = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ];
+    return months[monthNumber - 1]; // Bulan dimulai dari indeks 0
+}
+
+// Fungsi untuk menghitung jumlah data dengan bulan dan tahun yang sama
 function countDataByMonthAndYear(data) {
     const countsale = {};
     data.forEach(item => {
-        if (item.SALE_DATE) {
-            const saleDate = item.SALE_DATE.split(' ')[0]; // Ambil bagian tanggal saja (dd/mm/yyyy)
-            const [day, month, year] = saleDate.split('/'); // Pisahkan hari, bulan, dan tahun
+        if (item.SALEDATE) {
+            const saleDate = item.SALEDATE.split(' ')[0]; // Ambil bagian tanggal saja (dd/mm/yyyy)
+            const [year, month, day] = saleDate.split('-'); // Pisahkan hari, bulan, dan tahun
             const monthName = getMonthName(parseInt(month)); // Dapatkan nama bulan dari angka bulan
             const key = `${monthName} ${year}`; // Gabungkan bulan dan tahun sebagai kunci
             countsale[key] = (countsale[key] || 0) + 1; // Tambahkan jumlah data dengan kunci yang sama
         } else {
-            console.warn('Invalid SALE_DATE format:', item.SALE_DATE);
+            console.warn('Invalid SALE_DATE format:', item.SALEDATE);
         }
     });
 
@@ -61,27 +102,15 @@ function countDataByMonthAndYear(data) {
     sortedCountSale.forEach(([key, value]) => {
         orderedCountSale[key] = value;
     });
-
     return orderedCountSale;
 }
 
-    // Fungsi untuk membuat chart
-    function createChart(countsale) {
-        const line_1 = document.getElementById("line_1").getContext('2d');
-        const labels = Object.keys(countsale); // Ambil kunci dari objek countsale sebagai label
-        const dataValues = Object.values(countsale); // Ambil nilai dari objek countsale sebagai data
 
-        const Chart_1 = new Chart(line_1, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Data Penjualan',
-                    data: dataValues,
-                    fill: false,
-                    borderColor: 'rgb(75, 192, 192)',
-                }]
-            }
-        });
-    }
-});
+function DweliingsData(data) {
+    const result = data.filter(dataset => dataset.BUILDINGCLASSCATEGORY.includes("DWELLINGS"));
+    return result;
+}
+function FilterData(data, value) {
+    const result = data.filter(dataset => dataset.SALEDATE.includes(value));
+    return result;
+}
--- js/server.js
+++ js/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path'); // Import modul path untuk bekerja dengan path file
+const fs = require('fs').promises; // Menggunakan versi promise-based dari fs
+const path = require('path');
 const app = express();
 const port = 1000;
 
@@ -13,11 +13,21 @@ app.use((req, res, next) => {
 });
 
 // Definisikan endpoint untuk menyajikan data JSON
-app.get('/data', (req, res) => {
-    // Path file JSON yang benar sesuai dengan struktur proyek Anda
+app.get('/data', async (req, res) => {
     const filePath = path.join(__dirname, '../assets/data/dwellings.json');
     try {
-        const data = fs.readFileSync(filePath, 'utf8');
+        const data = await fs.readFile(filePath, 'utf8');
+        res.json(JSON.parse(data));
+    } catch (error) {
+        console.error('Error reading file:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/dataset', async (req, res) => {
+    const filePath = path.join(__dirname, '../assets/data/nyc.json');
+    try {
+        const data = await fs.readFile(filePath, 'utf8');
         res.json(JSON.parse(data));
     } catch (error) {
         console.error('Error reading file:', error);
